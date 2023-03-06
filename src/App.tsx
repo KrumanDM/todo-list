bda801624ceaad900aba5d75ad1bf4efd57eb79d
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-06 17:49:53 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
 import './App.css';
-import { Famous } from './Famous';
-
-type FilterType = 'all' | "Dollars" | 'RUBLS'
+import {Todolist} from './Todolist';
 
 function App() {
-    let [cars, useCars] = useState([
-        {manufacturer:'BMW', model:'m5cs'},
-        {manufacturer:'Mercedes', model:'e63s'},
-        {manufacturer:'Audi', model:'rs6'},
-      ]);
 
-    const [money, setMoney] = useState([
-        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
-        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
-        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
-      ])
-    
-    const [filter, setFilter] = useState<FilterType>('all');
-    
-    // по умолчанию эти переменные равны
-    let currentMoney = money;
-    // если nameButton === 'RUBLS'
-    if (filter === 'RUBLS') {
-    currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'RUBLS');
-    // если nameButton === 'Dollars'
-    }
-    if (filter === 'Dollars') {
-    currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'Dollars');
-    }
-
-    const onClickFilterHandler = (nameButton: FilterType) => {
-      setFilter(nameButton)
-    }
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
 
     return (
-        
-        <>
-            <ul>
-                {currentMoney.map((objFromMoneyArr,index) => {
-                    return (
-                      <li key={index}>
-                        <span>{objFromMoneyArr.banknots}</span>
-                        <span>{objFromMoneyArr.value}</span>
-                        <span>{objFromMoneyArr.number}</span>
-                      </li>
-                      )
-                })}
-            </ul>
-            <div style={{marginLeft:"35px"}}>
-              <button onClick={()=>onClickFilterHandler('all')}>all</button>
-              <button onClick={()=>onClickFilterHandler('RUBLS')}>ruble</button>
-              <button onClick={()=>onClickFilterHandler('Dollars')}>dollar</button>
-            </div>
-        
-        </>
-    )   
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1} />
+            <Todolist title="Songs" tasks={tasks2} />
+        </div>
+    );
 }
 
 export default App;
-{/* <Famous cars={cars} /> */}
