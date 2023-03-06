b63884eb25152dd141278036b6ad5150e73a609c
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-06 17:55:24 +0300

    .

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -19,10 +19,19 @@ export function Todolist(props: PropsType) {
             <button>+</button>
         </div>
         <ul>
+            {props.tasks.map((task: TaskType) => {
+                return (
+                    <li key={task.id}><input type="checkbox"  checked={task.isDone}/>
+                    <span>{task.title}</span>
+                        </li>
+                )
+            })}
+        </ul>
+        {/* <ul>
             <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
             <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
             <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
-        </ul>
+        </ul> */}
         <div>
             <button>All</button>
             <button>Active</button>
