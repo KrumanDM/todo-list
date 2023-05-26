7ab8097e3fc9ce3bc36b69c3dab28ff7fc6bc7e9
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-05-26 23:40:03 +0300

    .

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -31,6 +31,7 @@ export const Header: FC<PropsType> = ({handleOpen}) => {
                     className={s.burgerMenuIcon}
                     onClick={handleOpen}
                     alt={'open menu'}/>
+                    
                     {/* <h1>{"Skate Map"}</h1> */}
             </div>
         </>
